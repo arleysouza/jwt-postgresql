bb3aa73e44639d66419bdbbdec71b25c80ba2b11
arleysouza <arley.ferreira.souza@gmail.com>
2024-05-19 14:31:58 -0300

    codigo

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -2,38 +2,38 @@ import { Request, Response } from "express";
 import query from "../database/connection";
 
 class CategoryController {
-  public async create(req: Request, res: Response): Promise<Response> {
+  public async create(req: Request, res: Response): Promise<void> {
     const { name } = req.body;
     const r:any = await query(
       "INSERT INTO categories(name) VALUES ($1) RETURNING id, name",
       [name]
     );
-    return res.json(r);
+    res.json(r);
   }
 
-  public async list(_: Request, res: Response): Promise<Response> {
+  public async list(_: Request, res: Response): Promise<void> {
     const r:any = await query(
       "SELECT id,name FROM categories ORDER BY name"
     );
-    return res.json(r);
+    res.json(r);
   }
 
-  public async delete(req: Request, res: Response): Promise<Response> {
+  public async delete(req: Request, res: Response): Promise<void> {
     const { id } = req.body; 
     const r:any = await query(
       "DELETE FROM categories WHERE id = $1 RETURNING id, name", 
       [id]
     );
-    return res.json(r);
+    res.json(r);
   }
 
-  public async update(req: Request, res: Response): Promise<Response> {
+  public async update(req: Request, res: Response): Promise<void> {
     const { id, name } = req.body;
     const r:any = await query(
       "UPDATE categories SET name=$2 WHERE id=$1 RETURNING id, name", 
       [id,name]
     );
-    return res.json(r);
+    res.json(r);
   }
 }
 
