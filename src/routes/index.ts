7aa49c64a4869fb604cc04ea9695819f98951d51
arleysouza <arley.ferreira.souza@gmail.com>
2024-06-09 19:55:10 -0300

    codigo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from "express";
 import user from "./user";
 import product from "./product";
 import {UserController} from "../controllers/";
-import spent from "./expense";
+import expense from "./expense";
 import category from "./category";
 import { validadeAcess } from "../middlewares";
 
@@ -12,7 +12,7 @@ routes.post("/login", UserController.login);
 routes.use("/usuario", user);
 routes.use("/categoria", validadeAcess, category);
 routes.use("/produto", validadeAcess, product);
-routes.use("/gasto", validadeAcess, spent);
+routes.use("/gasto", validadeAcess, expense);
 
 //aceita qualquer método HTTP ou URL
 routes.use( (_:Request,res:Response) => res.json({message:"Requisição desconhecida"}) );
